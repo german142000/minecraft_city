b0ff94d86bce1db96d98da4308082707c3c5256b
Fonteyn German <germanbog74737@gmail.com>
2024-09-28 19:04:00 +0500

    Инициализация окна

--- minecraft_city/minecraft_city.cpp
+++ minecraft_city/minecraft_city.cpp
@@ -4,18 +4,64 @@
 #include <gl/GL.h>
 #include "GL/wglext.h" //Этот файл распространяется вне Windows SDK, поэтому его необходимо скачивать с сайта khronos.org
 
-int main()
-{
-    std::cout << "Hello World!\n";
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    HMODULE hLib;
+    void (*dpi)();
+    hLib = LoadLibrary("User32.dll");
+    (FARPROC&)dpi = GetProcAddress(hLib, "SetProcessDPIAware");
+    dpi();
+
+    const char* className = "OpenGLAppClass";
+    const char* windowTitle = "OpenGL";
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = className;
+    wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+
+    if (!RegisterClass(&wc)) {
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    HWND hWnd = CreateWindowEx(
+        WS_EX_APPWINDOW,
+        className,
+        windowTitle,
+        WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+        2,
+        2,
+        screenWidth,
+        screenHeight,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        return 0;
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+        break;
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+    return 0;
+}
