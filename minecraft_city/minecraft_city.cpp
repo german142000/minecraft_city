496d5e524baefffe2680d5db3ab02b7befe7587b
Fonteyn German <germanbog74737@gmail.com>
2024-09-28 19:09:40 +0500

    Инициализация контектса opengl

--- minecraft_city/minecraft_city.cpp
+++ minecraft_city/minecraft_city.cpp
@@ -4,6 +4,8 @@
 #include <gl/GL.h>
 #include "GL/wglext.h" //Этот файл распространяется вне Windows SDK, поэтому его необходимо скачивать с сайта khronos.org
 
+#pragma comment(lib, "glew/lib/Release/x64/glew32.lib")
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -42,6 +44,86 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         NULL,
         hInstance,
         NULL);
+
+    if (hWnd == NULL) {
+        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    HDC hdc = GetDC(hWnd);
+
+    PIXELFORMATDESCRIPTOR pfd;
+    ZeroMemory(&pfd, sizeof(pfd));
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 32;
+    pfd.cDepthBits = 24;
+    pfd.cStencilBits = 8;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+    if (!pixelFormat) {
+        CloseWindow(hWnd);
+        MessageBox(NULL, "Failed to choose pixel format.", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    if (!SetPixelFormat(hdc, pixelFormat, &pfd)) {
+        CloseWindow(hWnd);
+        MessageBox(NULL, "Failed to set pixel format.", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    HGLRC temphRC = wglCreateContext(hdc);
+    if (!temphRC) {
+        CloseWindow(hWnd);
+        MessageBox(NULL, "Failed to create OpenGL rendering context.", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    if (!wglMakeCurrent(hdc, temphRC)) {
+        CloseWindow(hWnd);
+        MessageBox(NULL, "Failed to activate OpenGL rendering context.", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = NULL;
+
+    wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
+
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(temphRC);
+
+    if (!wglCreateContextAttribsARB) {
+        CloseWindow(hWnd);
+        MessageBox(NULL, "wglCreateContextAttribsARB fail.", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    int attribs[] = {
+        WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+        WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+        WGL_CONTEXT_FLAGS_ARB,         WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
+        WGL_CONTEXT_PROFILE_MASK_ARB,  WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+        0
+    };
+
+    HGLRC hRC = wglCreateContextAttribsARB(hdc, 0, attribs);
+
+    if (!hRC || !wglMakeCurrent(hdc, hRC)) {
+        CloseWindow(hWnd);
+        MessageBox(NULL, "Creating render context fail.", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        CloseWindow(hWnd);
+        MessageBox(NULL, (const char*)glewGetErrorString(err), "GLEW error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
 
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
